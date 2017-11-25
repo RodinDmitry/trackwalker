63e9b1a8bc10d9d7d070b0e151e7ed075b826277
eshlykov <e.a.shlykov@gmail.com>
2017-11-25 21:24:54 +0300

    Добавил еще адаптер для списка задач

---
+++ app/src/main/java/com/example/eshlykov/trackwalker/Task.java
@@ -0,0 +1,45 @@
+package com.example.eshlykov.trackwalker;
+
+import android.os.Bundle;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by EvgenyShlykov on 25.11.2017, 025.
+ */
+
+public class Task {
+    private String taskName = "";
+    private Date startTime = new Date();
+    private Date finishTime = new Date();
+
+    DateFormat df = new SimpleDateFormat("HH:mm");
+
+    public Bundle getBundle() {
+        Bundle bundle = new Bundle();
+        bundle.putCharArray("taskName", taskName.toCharArray());
+//        bundle.putCharArray("sourceWord", startTime.toCharArray());
+//        bundle.putCharArray("translatedWord", finishTime.toCharArray());
+        return bundle;
+    }
+
+    public Task(Bundle bundle) {
+        taskName = new String(bundle.getCharArray("taskName"));
+    }
+
+    public Task(String _taskName, Date _startTime, Date _finishTime) {
+        taskName = _taskName;
+        startTime = _startTime;
+        finishTime = _finishTime;
+    }
+
+    public String getStartTime() {
+        return df.format(startTime);
+    }
+
+    public String getFinishTime() {
+        return df.format(finishTime);
+    }
+}
---
+++ app/src/main/java/com/example/eshlykov/trackwalker/TaskAdapter.java
@@ -0,0 +1,61 @@
+package com.example.eshlykov.trackwalker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by EvgenyShlykov on 25.11.2017, 025.
+ */
+
+public class TaskAdapter extends ArrayAdapter<Task> {
+    public TaskAdapter(Activity context, ArrayList<Task> tasks) {
+        super(context, 0, tasks);
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.task, parent, false);
+        }
+        final Task currentTask = getItem(position);
+
+        listItemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick( View view) {
+                Intent intent = new Intent(getContext(), TaskDetailedActivity.class);
+                Log.d("Diction", "intenting");
+                intent.putExtra("task", currentTask.getBundle());
+                getContext().startActivity(intent);
+            }
+        });
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.start_time);
+        defaultTextView.setText(currentTask.getStartTime());
+
+        TextView russianTextView = (TextView) listItemView.findViewById(R.id.finish_time);
+        russianTextView.setText(currentTask.getFinishTime());
+
+        // Вроде, будет нужно, если захотим удалять.
+//        Button deleteButton = (Button) listItemView.findViewById(R.id.delete_task);
+//        deleteButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                DataBase.getInstance(getContext()).delete(currentTask);
+//                TaskAdapter.this.remove(currentTask);
+//            }
+//        });
+
+        return listItemView;
+    }
+
+}
